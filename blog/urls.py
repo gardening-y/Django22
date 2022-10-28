6b29a6f7002a48e837bfcf7091a592e6c9f7fd9a
gardening-y <60008786@naver.com>
2022-10-28 20:16:51 +0900

    중간시험 이전

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ # IP주소/blog
+urlpatterns = [ # IP주소/blog/
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view())
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page) #함수를 부른 것
+
     # path('', views.index), # IP주소/blog
     # path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
+
     # 템플릿 모델명_list.html : post_list.html
     # index -> post_list 이름 바꾸기
     # 파라미터 모델명_list : post_list ('')있는거 'posts'
     
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data() #위 함수와 super 매개변수의 차이
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
+
+def category_page(request, slug):
+    if slug == 'no_category' :
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else :
+        category = Category.objects.get(slug=slug) #왼쪽은 필드명, 오른쪽은 찾기 원하는 슬러그 값
+        post_list = Post.objects.filter(category=category)
+    return render(request, 'blog/post_list.html', {
+        'category' : category, #템플릿 변수 : 함수 안에 category
+        'post_list' : post_list, #Post.objects.filter(category=category), 포스트가 가진 : 슬러그를 통해 전달받은 함수 안 변수
+        'categories' : Category.objects.all(),
+        'no_category_post_count' : Post.objects.filter(category=None).count
+    })
+
+
     # 템플릿 모델명_detail.html : post_detail.html
     # blog/single_post_page -> post_detail 이름 바꾸기
     # 파라미터 모델명 : post
