743b948944026be888641920243cbc2298eb936d
gardening-y <60008786@naver.com>
2022-11-03 14:44:19 +0900

    다대다 관계 구현하기 완료

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [ # IP주소/blog/
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('category/<str:slug>/', views.category_page) #함수를 부른 것
+    path('category/<str:slug>/', views.category_page), #함수를 부른 것 #IP주소/blog/category/slug/
+    path('tag/<str:slug>/', views.tag_page), #IP주소/blog/tag/slug/
 
     # path('', views.index), # IP주소/blog
     # path('<int:pk>/', views.single_post_page)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -38,6 +38,17 @@ def category_page(request, slug):
         'post_list' : post_list, #Post.objects.filter(category=category), 포스트가 가진 : 슬러그를 통해 전달받은 함수 안 변수
         'categories' : Category.objects.all(),
         'no_category_post_count' : Post.objects.filter(category=None).count
+    })
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(request, 'blog/post_list.html', {
+        'tag' : tag,
+        'post_list' : post_list,
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count
     })
 
 
